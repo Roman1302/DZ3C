98f7563105ac8b7bbc83f8a927297f5413e83e1c
Роман Бухмистов <rrb13@mail.ru>
2022-08-15 00:01:22 +0300

    Добавили 23 задание

---
+++ task23/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.WriteLine("Программа вывода кубов чисел от 1 до N");
+Console.WriteLine("Введите крайнее число для вычесления куба: ");
+
+string number = Console.ReadLine();
+int n = int.Parse(number);
+int a = 1;
+
+if (n < 0)
+    Console.WriteLine ("Введено не верное значение");
+else
+{
+    int power = 3;
+    for (int i = 1; i < n+1; i++)
+    {
+    Console.Write(Math.Pow(+ i, power) + " ");
+    }
+}
+Console.WriteLine();
