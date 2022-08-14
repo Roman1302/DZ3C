9bbba1e32161c2c8f882e4ffd368b26e2785bf43
Роман Бухмистов <rrb13@mail.ru>
2022-08-14 16:32:53 +0300

    Выполненно 19 задание

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,3 +3,23 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+Console.WriteLine("Программа проверки пятизначного числа на палиндромность");
+Console.WriteLine("Введите пятизначное число: ");
+
+string number = Console.ReadLine()!;
+int numberCheck = int.Parse(number!);
+
+Console.Write("\n");
+if (numberCheck < 10000 || numberCheck > 99999)
+{
+    Console.WriteLine("Неверно задано число");
+    return;
+}
+
+ else if (number![0] == number[4] && number[1] == number[3])
+
+    Console.WriteLine("ДА");
+
+else
+
+    Console.WriteLine("Нет");
